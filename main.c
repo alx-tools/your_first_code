9d045e5b31af5b01b1b7c2dbaa3acecf5fbf2790
viyo85 <novian1985@yahoo.co.id>
2016-09-04 02:57:55 -0400

    change main.c
    updated version

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
+
+#include <stdio.h>
+// insert code here...
 int main()
 {
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("Holberton\n");
-  }
-  return 0;
+    
+    char gender;
+    printf("Please select your gender [M/F]: ");
+    gender = getchar();
+    
+    printf("You selected: %c", gender);
+    getchar();
+    getchar();
+    
+    
+    int i;
+    for (i = 0; i < 32; i++)
+    {
+        printf("%s Holberton\n","Hello");
+    }
+    return 0;
 }
