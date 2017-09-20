e2ecf8353b5e96e17a1d9d87937885b337795435
rachel98rl <rachel98rl@hotmail.com>
2017-09-19 21:15:42 -0400

    Updated!

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("Holberton\n");
+int main(){
+  for (int i = 0; i < 10; i++){
+	  if (i == 2) {
+    printf("Hello World!");
+	printf("This is easy code");
+	  }
   }
   return 0;
 }
+
+
