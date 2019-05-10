60b2007c4c90175c99f1a771d9abdb13bc652b9f
Josephinebt <49733335+Josephinebt@users.noreply.github.com>
2019-05-10 10:51:14 -0400

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,40 @@ int main()
   int i;
   for (i = 0; i < 10; i++)
   {
-    printf("Holberton\n");
+    printf("Hello Holberton\n");
   }
   return 0;
+} 
+printf("Hello Holberton\n");
 }
+ return 0;
+
+printf("Hello Holberton\n");
+}
+printf("Hello Holberton\n");
+}
+printf("Hello Holberton\n");
+}
+printf("Hello Holberton\n");
+}
+printf("Hello Holberton\n");
+}
+printf("Hello Holberton\n");
+}
+printf("Hello Holberton\n");
+}
+printf("Hello Holberton\n");
+}
+printf("Hello Holberton\n");
+}
+printf("Hello Holberton\n");
+}
+printf("Hello Holberton\n");
+}
+printf("Hello Holberton\n");
+}
+printf("Hello Holberton\n");
+}
+printf("Hello Holberton\n");
+}
+printf("Display \n");
