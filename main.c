8979cf92e199b97c9bcc327fbe253ec5d3578579
i1shankar <52138311+i1shankar@users.noreply.github.com>
2019-06-25 10:12:42 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 int main()
 {
-  int i;
-  for (i=0; i < 32; i++)
+  int i"
+  for (i = 0; i < 32; i++)
   {
-    printf("Hello Holberton\n");
+   printf("Hello Holberton\n");
   }
   return 0;
-}
+  
