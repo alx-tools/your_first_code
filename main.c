d1af8b97de130f2212dc752bbbbcd7a29a0d8862
Aymen33 <53335911+Aymen33@users.noreply.github.com>
2019-07-26 12:02:34 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 32; i++)
   {
-    printf("Holberton\n");
+    printf("Hello Holberton\n");
   }
   return 0;
 }
+git commit -m " the code now displays Hello Holberton instead of Holberton, and does it 32 times instead of 10 "
