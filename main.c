5efcb988b58b58091b062b27e4e2134e717da31e
Deepika309 <36249665+Deepika309@users.noreply.github.com>
2019-10-15 10:05:58 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
 int main()
 {
   int i;
   for (i = 0; i < 10; i++)
   {
-    printf("Holberton\n");
+    printf("Holberton\n");  /*prints the word "Holberton" 10 times in 10 different lines*/
   }
   return 0;
 }
