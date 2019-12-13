3a0564c2ce0d5b03f5cfe409c9c1e6e040de4454
TiouiriMokhtar <58710231+TiouiriMokhtar@users.noreply.github.com>
2019-12-13 20:36:04 +0100

    main.c
    Mokhtar Tiouiri

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
-int main()
+int  main ()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  pour (i = 0 ; i < 32 ; i ++)
   {
-    printf("Holberton\n");
+    printf ( " Hello Holberton \ n " );
   }
-  return 0;
+  retourner  0 ;
 }
