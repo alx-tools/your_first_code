12a0cea91c47c2379a9cf0775d33f36928a8e8ef
jtogunto <59484998+jtogunto@users.noreply.github.com>
2020-01-05 21:19:33 -0600

    Update main.c
    Hello Holberton

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
+#include <stdio.h>
 int main()
 {
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("Holberton\n");
-  }
-  return 0;
+int i;
+for (i = 0; i < 10; i++)
+for (i = 0; i < 32; i++)
+{
+
+printf("Hello Holberton\n");
+}
+return 0;
 }
