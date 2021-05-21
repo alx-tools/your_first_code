9c88101663ad3ace3e3189ca4ffb2f8895b389b3
Jayla Williams <84402860+jaylaevelyn@users.noreply.github.com>
2021-05-21 09:51:04 -0500

    Main c
    int main()
    {
    int i;
    for (i = 0; i < 32; i++)
    {
    printf("Hello School\n");
    }
    return 0;
    }

--- Main.c
+++ Main.c
@@ -1,21 +1,11 @@
 # your_first_code
-A very simple snippet of C code, for Holberton School's admissions.
+int main()
+{
+int i;
+for (i = 0; i < 32; i++)
+{
+printf("Hello School\n");
+}
+return 0;
+}
 
-int main()
-{
-int i;
-for (i = 0; i < 32; i++)
-{
-printf("Hello School\n");
-}
-return 0;
-}
-int main()
-{
-int i;
-for (i = 0; i < 32; i++)
-{
-printf("Hello School\n");
-}
-return 0;
-}
