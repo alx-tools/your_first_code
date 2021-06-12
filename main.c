93a261b3a4025f4bfcb82b5f02dd56b5b4e18900
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-06-12 15:08:25 -0400

    adustments were made to the for loop, "school" is now "Hello School" and it prints 32 times

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 32; i++)
   {
-    printf("School\n");
+    printf(" Hello School\n");
   }
   return 0;
 }
+//not sure if this works, I have never used C but I am familiar with loops.
