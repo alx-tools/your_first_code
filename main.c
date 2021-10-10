3d0c886e267e0a0814dc7568a90358d318efea5f
JRA2880 <johnrusselladamsjr@gmail.com>
2021-10-09 21:06:41 -0400

    Update to the Master Branch

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
+
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i <= 32; i++)
   {
-    printf("School\n");
+    printf("Hello School\n");
   }
   return 0;
 }
